aa5fb83a2dab2d10ff9d9a9bfe38a995038aada5
flm_yk012 <firstlinuxmint@gmail.com>
2020-03-26 16:35:50 +0900

    add basic settings el file

---
+++ init/05-basic.el
@@ -0,0 +1,29 @@
+;;
+;; basic settings
+;;
+
+;; time outline
+(display-time)
+
+;; line number
+(use-package linum
+  :config
+  (global-display-line-numbers-mode t))
+
+;; make line number easir to understand
+(set-face-attribute 'linum nil
+		    :foreground "#a9a9a9"
+		    :background "#404040"
+		    :height 0.9)
+
+
+;; font
+(custom-set-faces
+'(default ((t (:family "Cica" :foundry "TMNM" :slant normal :weight normal :height 128 :width normal)))))
+
+;; hide meny and tool bar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+
+;; highlight corresponding parentheses
+(show-paren-mode t)
