e40b204054e1cc67cae92a049a4cd68f391b8be7
flm_yk012 <firstlinuxmint@gmail.com>
2020-03-26 16:40:36 +0900

    delete font setting

--- init/01-character-code.el
+++ init/01-character-code.el
@@ -7,6 +7,3 @@
 (prefer-coding-system 'utf-8)
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
-
-;; font
-(set-fontset-font t 'japanese-jisx0208 "SourceCodePro-Regular")
