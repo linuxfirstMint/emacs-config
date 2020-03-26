df345601ab53d7616076368f5a7a355f871e0c7c
flm_yk012 <firstlinuxmint@gmail.com>
2020-03-26 16:01:04 +0900

    update

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 
 ;;Package management
 (require 'package)
+(add-to-list 'package-archives '("gnu"	      . "http://elpa.gnu.org/packages/")t)
 (add-to-list 'package-archives '("melpa"	      . "http://melpa.org/packages/")t)
 (add-to-list 'package-archives '("melpa-stable"      . "http://stable.melpa.org/packages/")t)
 (add-to-list 'package-archives '("org"	              . "http://orgmode.org/elpa/")t)
